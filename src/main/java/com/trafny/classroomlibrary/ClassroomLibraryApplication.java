2e8edf8efa594cdca8db20814ae1b6b16edb9db1
itmightbeme <beccatrafny@gmail.com>
2025-04-21 21:35:53 -0600

    Initial Commit

---
+++ src/main/java/com/trafny/classroomlibrary/ClassroomLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.trafny.classroomlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassroomLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassroomLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trafny/classroomlibrary/ClassroomLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trafny.classroomlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassroomLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
