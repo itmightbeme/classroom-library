0665b58b0f932e1e3b90244f5f5a5865f404ecf1
itmightbeme <beccatrafny@gmail.com>
2025-06-06 18:06:45 -0600

    add book information fragment

--- src/main/java/com/trafny/classroomlibrary/Controllers/BookController.java
+++ src/main/java/com/trafny/classroomlibrary/Controllers/BookController.java
@@ -1,12 +1,19 @@
 package com.trafny.classroomlibrary.Controllers;
 
+import com.trafny.classroomlibrary.Entities.Book;
 import com.trafny.classroomlibrary.Entities.BookCopy;
+import com.trafny.classroomlibrary.Entities.Genre;
 import com.trafny.classroomlibrary.Repositories.BookCopyRepo;
+import com.trafny.classroomlibrary.Repositories.BookRepo;
+import com.trafny.classroomlibrary.Repositories.GenreRepo;
+import com.trafny.classroomlibrary.Repositories.TopicRepo;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Comparator;
 import java.util.List;
@@ -16,7 +23,16 @@ import java.util.List;
 public class BookController {
 
     @Autowired
+    private BookRepo bookRepo;
+
+    @Autowired
     private BookCopyRepo bookCopyRepo;
+
+    @Autowired
+    private GenreRepo genreRepo;
+
+    @Autowired
+    private TopicRepo topicRepo;
 
     @GetMapping("/inventory")
     public String showInventory(Model model) {
@@ -26,13 +42,22 @@ public class BookController {
         return "books/inventory";
     }
 
+    //new controllers
 
 
+    @GetMapping("/detail/{id}")
+    public String showBookDetail(@PathVariable Long id, Model model) {
+        Book book = bookRepo.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid book ID: " + id));
+        model.addAttribute("book", book);
+        return "books/detail";
+    }
 
 
-
-
-
-
-
+    @GetMapping("/add")
+    public String showAddBookForm(Model model) {
+        model.addAttribute("book", new Book());
+        model.addAttribute("genres", genreRepo.findAll());
+        model.addAttribute("topics", topicRepo.findAll());
+        return "books/detail";
+    }
 }
--- src/main/java/com/trafny/classroomlibrary/Repositories/GenreRepo.java
+++ src/main/java/com/trafny/classroomlibrary/Repositories/GenreRepo.java
@@ -3,5 +3,8 @@ package com.trafny.classroomlibrary.Repositories;
 import com.trafny.classroomlibrary.Entities.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GenreRepo extends JpaRepository<Genre, Long> {
+    Optional<Genre> findByNameIgnoreCase(String cleaned);
 }
