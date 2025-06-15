93152a92cce5816e57a99c58421555d469cea669
itmightbeme <beccatrafny@gmail.com>
2025-06-15 14:31:45 -0600

    convert student list to fragment

--- src/main/java/com/trafny/classroomlibrary/Controllers/StudentController.java
+++ src/main/java/com/trafny/classroomlibrary/Controllers/StudentController.java
@@ -34,7 +34,7 @@ public class StudentController {
     }
 
     // Show form to add new student
-    @GetMapping("/detail")
+    @GetMapping("/add")
     public String showAddStudentForm(Model model) {
         model.addAttribute("student", new Student());
         return "teachers/students/detail";
